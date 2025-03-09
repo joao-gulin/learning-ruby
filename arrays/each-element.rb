cf2c2420bb1fc6fcc763771acad5a69e93c6ea01
joao-gulin <joaogulin@gmail.com>
2025-03-09 01:15:39 +0000

    array learning

---
+++ arrays/each-element.rb
@@ -0,0 +1,5 @@
+fruits = %w[Banana Orange Pineapple Watermelon]
+
+for fruit in fruits
+  puts "Fruits in the Array: #{fruit}"
+end
---
+++ arrays/sum-elements.rb
@@ -0,0 +1,10 @@
+numbers = [1, 2, 4, 4, 5, 10]
+total = 0
+
+numbers.push(20)
+
+for number in numbers
+  total += number
+end
+
+puts "The total sum from the array: #{total}"
